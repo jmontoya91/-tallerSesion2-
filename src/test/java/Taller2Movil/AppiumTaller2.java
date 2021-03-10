83381034ff92a59e6791e319c2c26673741885d9
Joe <joemeudes@gmail.com>
2021-03-10 15:05:29 -0500

    Pruebas Basicas

--- src/test/java/Taller2Movil/AppiumTaller2.java
+++ src/test/java/Taller2Movil/AppiumTaller2.java
@@ -7,10 +7,13 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 
@@ -19,14 +22,15 @@ public class AppiumTaller2 {
 
     // creamos el Driver Appium
     private AppiumDriver driver;
-
+    String Titulo="Prueba";
+    String Titulo2="Prueba2";
         @Before
     public void Before() throws MalformedURLException {
             DesiredCapabilities ConfiguracionAppium =new DesiredCapabilities();
             ConfiguracionAppium.setCapability("deviceName","HUAWEI P20 lite");
             ConfiguracionAppium.setCapability("platformVersion","9");
-            ConfiguracionAppium.setCapability("appPackage","com.android.calculator2");
-            ConfiguracionAppium.setCapability("appActivity","Calculator");
+            ConfiguracionAppium.setCapability("appPackage","com.vrproductiveapps.whendo");
+            ConfiguracionAppium.setCapability("appActivity",".ui.HomeActivity");
             ConfiguracionAppium.setCapability("platformName","Android");
 
             // driver apunte a nuestro appiumDesktop
@@ -34,35 +38,42 @@ public class AppiumTaller2 {
 
             // implicit:
             driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
-
-
         }
+
+    public void Crear_Nota(){
+            driver.findElement(By.xpath("//android.widget.ImageButton[@resource-id='com.vrproductiveapps.whendo:id/fab']")).click();
+            driver.findElement(By.xpath("//android.widget.EditText[@resource-id='com.vrproductiveapps.whendo:id/noteTextTitle']")).sendKeys(Titulo);
+            driver.findElement(By.xpath("//android.widget.EditText[@index='2']")).sendKeys("Prueba 1");
+            driver.findElement(By.xpath("//android.widget.TextView[@content-desc='Guardar']")).click();
+      }
+    public void Editar_Nota(){
+        Crear_Nota();
+        driver.findElement(By.xpath("//android.widget.TextView[@text='"+Titulo+"']")).click();
+        driver.findElement(By.xpath("//android.widget.EditText[@resource-id='com.vrproductiveapps.whendo:id/noteTextTitle']")).sendKeys(Titulo2);
+        driver.findElement(By.xpath("//android.widget.EditText[@index='2']")).sendKeys("Prueba Editada");
+        driver.findElement(By.xpath("//android.widget.TextView[@content-desc='Guardar']")).click();
+    }
 
 
     @Test
-    public void Verificar_calcular(){
-        // elemento 1
-            driver.findElement(By.id("com.android.calculator2:id/digit_9")).click();
-        // suma
-          driver.findElement(By.id("com.android.calculator2:id/op_add")).click();
-        // elemento 2
-        driver.findElement(By.xpath("//android.widget.Button[@text='2']")).click();
-         // igual
-        driver.findElement(By.id("com.android.calculator2:id/eq")).click();
-        String expectedResult="13";
-
-        String actualResult= driver.findElement(By.id("com.android.calculator2:id/formula")).getText();
-        Assert.assertEquals("Error el resulta es incorrecto",expectedResult,actualResult);
-
+    public void Validar_Existencia_Nota1(){
+            Crear_Nota();
+        int Notas=driver.findElements(By.xpath("//android.widget.TextView[@text='"+Titulo+"']")).size();
+        Assert.assertEquals("ERROR en contacto",1,Notas);
+    }
+    @Test
+    public void Validar_Edicion_Nota2() {
+        Editar_Nota();
+        String expectedResult="Prueba2";
+        String ResultadoReal=driver.findElement(By.xpath("//android.widget.TextView[@text='"+Titulo2+"']")).getText();
+        Assert.assertEquals("Error el resulta es incorrecto",expectedResult,ResultadoReal);
     }
 
 
 
 
 
-
-    @After
+        @After
     public void After(){
         driver.quit();
 
